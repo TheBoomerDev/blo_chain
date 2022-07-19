a38a622b5dbc595cf919d2b7b73f07eacd6c0eac
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-07-19 18:03:24 +0200

    Initial

---
+++ src/test/java/com/blochain/api/BloChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blochain.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
